6c068311c10bb59ed84a1ac6debffe52f30fda51
Max Grechko <max@grechko.org>
2015-05-09 21:58:20 +0300

    Add rspec

--- spec/rglib_spec.rb
+++ spec/rglib_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
 describe Rglib do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Rglib::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  subject(:author) { described_class.new("Chuck Norris", "Cool man") }
+
+  describe Rglib::Author do
+    it "should respond to name" do
+      expect(author.name).to eq("Chuck Norris")
+    end
+
+    it "should respond to biography" do
+      expect(author.biography).to eq("Cool man")
+    end
+
+    it "should respond to to_s method" do
+      expect(author.to_s).to eq("Chuck Norris. Cool man")
+    end
+  end
+
+  describe Rglib::Book do
+    subject(:book) do
+      described_class.new("101 of Chuck's Favorite Facts and Stories", author)
+    end
+
+    it "should respond to title" do
+      expect(book.title).to eq("101 of Chuck's Favorite Facts and Stories")
+    end
+
+    it "should respond to author" do
+      expect(book.author).to eq(author)
+    end
   end
 end
